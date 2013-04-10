7f75ec5e49bb95f4afedd827c60cdddb15f46970
Christian Eder <ederc@mathematik.uni-kl.de>
2013-04-10 10:50:25 +0200

    1st working sequential cache-oblivious GEP implementation: Restrictions: - Size of Matrix 2^n x 2^n               - No pivoting

--- src/seq/mat-elim-seq.cpp
+++ src/seq/mat-elim-seq.cpp
@@ -14,51 +14,42 @@
 void elimCoSEQBaseModP( mat *M, const uint32 k1, const uint32 i1, 
                         const uint32 j1, const uint32 rows, const uint32 cols,
                         uint64 size, uint64 prime, mat *neg_inv_piv) {
-  mat **Mpiv, **Mmdf;
   uint64 k;
- 
+    
   // get pivots first, those do only depend on the value of k1 and the size,
   // i.e. the length of the diagonal of the block of M 
-  uint64 sizeMat  = size * sizeof(mat*);
-  Mpiv            = (mat **)malloc(sizeMat);
-  sizeMat     = size * sizeof(mat);
   for (k = 0; k < size; k++) {
-    Mpiv[k] = (mat *)malloc(sizeMat);
-    memcpy(Mpiv[k], &M[k1+(k1+k)*cols], sizeMat);
-  }
-  printf("- - - Size %d - k1 %d - i1 %d - j1 %d - - - \n", size, k1, i1, j1);
-  for (int it=0; it < size; ++it) {
-    for (int jt=0; jt < size; ++jt) {
-      printf("Mpiv[%d][%d] = %u\n", it, jt, Mpiv[it][jt]);
-    }
+    //Mpiv[k] = (mat *)malloc(sizeMat);
+    //memcpy(Mpiv[k], &M[k1+(k1+k)*cols], sizeMat);
   }
   // now check in which part of M we are: is this the part the diagonal from k1
   // to k1+size is running? if so we know that the pivots are the elements and
   // we can just set the pointer Mmdf = Mpiv
   if (i1 == k1 && j1 == k1) {
-    Mmdf  = Mpiv;
+    //Mmdf  = Mpiv;
   // otherwise, we need to store the right matrix elements in Mmdf. Those
   // elements are completely different from the ones in Mpivot (remember that we
   // have subdivided M in 4 parts. Mpiv is running in 1 part, Mmdf in one of the
   // remaining 3 parts
   } else {
-    sizeMat  = size*sizeof(mat*);
-    Mmdf            = (mat **)malloc(sizeMat);
-    sizeMat         = size*sizeof(mat);
-    for (k = 0; k < size; k++) {
-      Mmdf[k] = (mat *)malloc(sizeMat);
-      memcpy(Mmdf[k], &M[j1+(i1+k)*cols], sizeMat);
-    }
+    //sizeMat  = size*sizeof(mat*);
+    //Mmdf            = (mat **)malloc(sizeMat);
+    //sizeMat         = size*sizeof(mat);
+    //for (k = 0; k < size; k++) {
+    //  Mmdf[k] = (mat *)malloc(sizeMat);
+    //  memcpy(Mmdf[k], &M[j1+(i1+k)*cols], sizeMat);
+    //}
   }
 
   for (k = 0; k < size; k++) {
-    Mpiv[k][k] %= prime;
-    const mat *Mpivk  = Mpiv[k];
+    M[(k1+k)+(k1+k)*cols] %= prime;
+    //const mat *Mpivk  = Mpiv[k];
     // possibly the negative inverses of the pivots at place (k,k) were already
     // computed in another call. otherwise we need to compute and store it
     if (!neg_inv_piv[k+k1]) {
-      if (Mpivk[k] != 0)
-        neg_inv_piv[k+k1] = negInverseModP(Mpivk[k], prime);
+      if (M[(k1+k)+(k1+k)*cols] != 0) {
+        neg_inv_piv[k+k1] = negInverseModP(M[(k1+k)+(k1+k)*cols], prime);
+      }
     }
     const mat inv_piv   = neg_inv_piv[k+k1];
     // if the pivots are in the same row part of the matrix as Mmdf then we can
@@ -67,36 +58,17 @@ void elimCoSEQBaseModP( mat *M, const uint32 k1, const uint32 i1,
     const uint32 istart  = (k1 == i1) ? k+1 : 0;
     uint64 i;
     for (i = istart; i < size; i++) {
-      mat *Mmdfi    = Mmdf[i];
-      const mat tmp = (Mmdfi[k] * inv_piv) % prime;
+      const mat tmp = (M[k+k1+(i1+i)*cols] * inv_piv) % prime;
       // if the pivots are in the same column part of the matrix as Mmdf then we can
       // always start at the next column (k+1), otherwise we need to start at
       // column 0
       const uint32 jstart  = (k1 == j1) ? k+1 : 0;
       uint64 j;
       for (j = jstart; j < size; j++) {
-  	    Mmdfi[j]  +=  Mpivk[j] * tmp;
-  	    Mmdfi[j]  %=  prime;
+  	    M[(j1+j)+(i1+i)*cols]  +=  M[(j1+j)+(k1+k)*cols] * tmp;
+  	    M[(j1+j)+(i1+i)*cols]  %=  prime;
       }
     }
-  }
-
-    sizeMat = size * sizeof(mat);
-    for (k = 0; k < size; k++) {
-      memcpy(&M[k1+(k1+k)*cols], Mpiv[k], sizeMat);
-      //memcpy(M[k1+k]+k1, Mpiv[k], sizeMat);
-      free(Mpiv[k]);
-    }
-    free(Mpiv);
-  if (i1 != k1 || j1 != k1) {
-    sizeMat = size*sizeof (mat);
-    for (k = 0; k < size; k++) {
-      memcpy(&M[j1+(i1+k)*cols], Mmdf[k], sizeMat);
-      //memcpy(M[i1+k]+j1, Mmdf[k], sizeMat);
-      printf("size of mmdf %d\n", sizeof(Mmdf[k]));
-      free(Mmdf[k]);
-    }
-    free(Mmdf);
   }
 }
 
@@ -118,7 +90,6 @@ void elimCoSEQBlockModP(mat *M, const uint32 k1, const uint32 k2,
     uint32 km = (k1+k2) / 2 ;
     uint32 im = (i1+i2) / 2;
     uint32 jm = (j1+j2) / 2;
-    printf("size %d -- km %d - im %d - jm %d\n",size, km, im, jm);
 
     elimCoSEQBlockModP( M, k1, km, i1, im, j1, jm, rows, cols, size,
                         prime, neg_inv_piv);
@@ -144,15 +115,13 @@ void elimCoSEQModP(Matrix& A, int blocksize, uint64 prime) {
   uint32 n          = A.nCols();
   // if m > n then only n eliminations are possible
   uint32 boundary   = (m > n) ? n : m;
-  uint64 size       = m * n;
   //mat *a_entries    = (mat *)malloc(A.entries.size() * sizeof(mat));
   //memcpy(a_entries, A.entries.data(), A.entries.size() * sizeof(mat));
   mat *a_entries    = A.entries.data();
   mat *neg_inv_piv  =   (mat *)calloc(boundary, sizeof(mat));
+  cleanUpModP(A, prime);
   a_entries[0]      %=  prime;
-  A.print();
   neg_inv_piv[0]    =   negInverseModP(a_entries[0], prime); 
-  mat inv, mult;
   timeval start, stop;
   clock_t cStart, cStop;
   std::cout << "Cache-oblivious Gaussian Elimination without pivoting" << std::endl;
@@ -201,7 +170,6 @@ void elimCoSEQModP(Matrix& A, int blocksize, uint64 prime) {
 }
 
 void elimNaiveSEQModP(Matrix& A, int blocksize, uint64 prime) {
-  uint32 l;
   uint32 m         = A.nRows();
   uint32 n         = A.nCols(); 
   // if m > n then only n eliminations are possible
